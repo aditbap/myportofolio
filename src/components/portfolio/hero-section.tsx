a4636393444ccbdca0181b68796085b7d0da11d8
Benediktus Aditya Pratama <aditbaps@gmail.com>
2025-06-10 10:25:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="intro

--- src/components/portfolio/hero-section.tsx
+++ src/components/portfolio/hero-section.tsx
@@ -8,7 +8,7 @@ const HeroSection: React.FC = () => {
       className="container mx-auto px-4 sm:px-6 lg:px-8 text-left min-h-[calc(100vh-7rem)] sm:min-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-8rem)]"
     >
       <div className="max-w-3xl"> {/* Ensure this div allows text to align under navbar based on main's padding */}
-        <p className="font-jakarta font-extrabold text-3xl sm:text-4xl md:text-4xl text-foreground mb-1 sm:mb-2 animate-fadeInUp">
+        <p className="font-jakarta font-extrabold text-2xl sm:text-3xl md:text-4xl text-foreground mb-1 sm:mb-2 animate-fadeInUp">
           i&apos;m rocky <span role="img" aria-label="victory hand">✌️</span>
         </p>
         <p className="font-jakarta font-extrabold text-4xl sm:text-5xl md:text-6xl text-foreground mb-1 sm:mb-2 animate-fadeInUp" style={{ animationDelay: '0.2s' }}>
