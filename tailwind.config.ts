7d09399a75e14419e658af491ba0d15427d0a6e5
Benediktus Aditya Pratama <aditbaps@gmail.com>
2025-06-10 12:52:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="font-jak

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,6 +104,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fadeInUp': 'fadeInUp 0.5s ease-out forwards',
+        'fadeInUpBack': 'fadeInUp 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards',
       },
     },
   },
