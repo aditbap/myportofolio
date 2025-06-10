2e62ba60f786d3a155a61042edf6ca9a62d7edaf
Benediktus Aditya Pratama <aditbaps@gmail.com>
2025-06-10 10:54:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="px-6 p

--- src/components/portfolio/hero-section.tsx
+++ src/components/portfolio/hero-section.tsx
@@ -7,12 +7,12 @@ const HeroSection: React.FC = () => {
       id="intro"
       className="w-full max-w-3xl sm:max-w-4xl md:max-w-5xl mx-auto px-4 text-left min-h-[calc(100vh-7rem)] sm:min-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-8rem)]"
     >
-      <div className="px-6 pt-24"> {/* Changed pt-16 to pt-24 */}
+      <div className="px-6 pt-48"> {/* Increased pt-44 to pt-48 */}
         <p className="font-jakarta font-extrabold text-2xl sm:text-3xl md:text-4xl text-foreground mb-1 sm:mb-2 animate-fadeInUp">
           i&apos;m adit <span role="img" aria-label="victory hand">✌️</span>
         </p>
         <p
-          className="font-jakarta font-extrabold text-3xl sm:text-5xl md:text-6xl text-foreground mb-1 sm:mb-2 animate-fadeInUp"
+          className="font-jakarta font-extrabold text-3xl sm:text-4xl md:text-5xl text-foreground mb-1 sm:mb-2 animate-fadeInUp"
           style={{ animationDelay: '0.2s' }}
         >
           student at computer science @scrolldown through prototypes
