6dfacf392965dadb8778c3523504b0f57cf3cf4d
Benediktus Aditya Pratama <aditbaps@gmail.com>
2025-06-10 09:59:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="intro

--- src/components/portfolio/hero-section.tsx
+++ src/components/portfolio/hero-section.tsx
@@ -8,13 +8,13 @@ const HeroSection: React.FC = () => {
       className="container mx-auto px-4 sm:px-6 lg:px-8 text-left min-h-[calc(100vh-7rem)] sm:min-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-8rem)]"
     >
       <div className="max-w-3xl"> {/* Removed pt-16 */}
-        <h1 className="font-jakarta text-3xl sm:text-4xl md:text-4xl text-foreground mb-1 sm:mb-2 animate-fadeInUp">
+        <h1 className="font-jakarta text-3xl sm:text-4xl md:text-4xl font-bold text-foreground mb-1 sm:mb-2 animate-fadeInUp">
           i&apos;m rocky <span role="img" aria-label="victory hand">✌️</span>
         </h1>
         <p className="font-jakarta text-4xl sm:text-5xl md:text-6xl font-bold text-foreground mb-1 sm:mb-2 animate-fadeInUp" style={{ animationDelay: '0.2s' }}>
           product designer unfolding stories
         </p>
-        <p className="font-jakarta text-2xl sm:text-3xl md:text-3xl text-foreground/80 animate-fadeInUp" style={{ animationDelay: '0.4s' }}>
+        <p className="font-jakarta text-2xl sm:text-3xl md:text-3xl font-bold text-foreground animate-fadeInUp" style={{ animationDelay: '0.4s' }}>
           @instagram through prototypes
         </p>
       </div>
