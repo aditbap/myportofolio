3b0a329ef1c01090619507748951f62001765bb9
Benediktus Aditya Pratama <aditbaps@gmail.com>
2025-06-10 10:20:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="intro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth dark"> {/* Added dark class here */}
+    <html lang="en" className="scroll-smooth dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Space+Grotesk:wght@400;700&family=Source+Code+Pro:wght@400&family=Plus+Jakarta+Sans:wght@400;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Space+Grotesk:wght@400;700&family=Source+Code+Pro:wght@400&family=Plus+Jakarta+Sans:wght@400;700;800&display=swap" rel="stylesheet"/>
       </head>
       <body className="font-body antialiased">
         {children}
